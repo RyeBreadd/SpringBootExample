395d5ea50cea2547d865715e6bec87008ec1f776
Ryan <grossry@oregonstate.edu>
2019-07-15 23:58:23 -0400

    Added driver to DB config

--- src/main/java/com/rgross/springbootexample/config/DatabaseConfig.java
+++ src/main/java/com/rgross/springbootexample/config/DatabaseConfig.java
@@ -23,6 +23,7 @@ public class DatabaseConfig {
     @Bean
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
+        config.setDriverClassName("com.mysql.jdbc.Driver");
         config.setJdbcUrl(databaseUrl);
         config.setUsername(username);
         config.setPassword(password);
