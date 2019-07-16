7421efdc3302f2edd625e5157fbd1a9abfebeed6
Ryan <grossry@oregonstate.edu>
2019-07-15 23:16:28 -0400

    Fixed controller annotations, switched from CrudRepository to JPARepository to use findAll method

--- src/main/java/com/rgross/springbootexample/repository/PersonRepository.java
+++ src/main/java/com/rgross/springbootexample/repository/PersonRepository.java
@@ -1,12 +1,9 @@
 package com.rgross.springbootexample.repository;
 
 import com.rgross.springbootexample.model.Person;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface PersonRepository extends CrudRepository<Person, Long> {
-    List<Person> findAll();
+public interface PersonRepository extends JpaRepository<Person, Long> {
 }
--- src/main/java/com/rgross/springbootexample/service/PersonService.java
+++ src/main/java/com/rgross/springbootexample/service/PersonService.java
@@ -1,23 +1,10 @@
 package com.rgross.springbootexample.service;
 
 import com.rgross.springbootexample.model.Person;
-import com.rgross.springbootexample.repository.PersonRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class PersonService {
-
-    @Autowired
-    PersonRepository personRepository;
-
-    public List<Person> getAllPeople() {
-        return personRepository.findAll();
-    }
-
-    public void addPerson(Person person) {
-        personRepository.save(person);
-    }
+public interface PersonService {
+    List<Person> getAllPeople();
+    void addPerson(Person person);
 }
